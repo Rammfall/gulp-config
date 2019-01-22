fee2fc828f747c806bc002155a4d30eead737128
Andrey Barchuk <andreyb@123software.ru>
2019-01-22 16:24:17 +0200

    deleted webpack =)

--- dev/js/index.js
+++ dev/js/index.js
@@ -1,5 +1,7 @@
-console.log('js maasdasdad a dasdin');
-let omgwtf = 233221311232123121233321;
+
+
+console.log('asdas asd aasdassd d ', a);
+let omgwtf = 2332213121321233321; 
 
 let arr = ["name", "sername"]
 
@@ -7,9 +9,28 @@ let [firstName, surname] = arr;
 
 console.log(firstName, surname);
 
-fetch('https://test.com').then(function(response) {
-  return response.blob();
+(function a (a = 'adas', b = 23213, c = undefined) {
+ console.log(a);
+})();
+
+fetch('https://api.chucknorris.io/jokes/random').then(function(response) {
+  console.log('test1')
+  return response.json();
 }).then(function(myBlob) {
-  var objectURL = URL.createObjectURL(myBlob);
-  console.log(objectURL);
+  console.log('test2')
+  console.log(myBlob.value);
+  
 });
+
+
+class Test {
+
+
+
+  run = () => {
+  
+    console.log('run')
+  }
+}
+
+new Test('asda', 'asdas', 'aaa');
--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,8 @@
-
 const autoprefixer = require('autoprefixer');
 const browser = require('browser-sync');
 const concat = require('gulp-concat');
 const cssnano = require('cssnano');
-const  del = require('del');
+const del = require('del');
 const gulp = require('gulp');
 const html = require('gulp-htmlmin');
 const image = require('gulp-image');
@@ -17,6 +16,8 @@ const babel = require('gulp-babel');
 const compiler = require('webpack');
 const webpack = require('webpack-stream');
 const gulpif = require('gulp-if');
+
+
 
 let {
   argv
@@ -74,36 +75,55 @@ let jsFiles = ['dev/scripts/first.js', 'dev/scripts/second.js'];
 //CSS task with source maps
 function devCSS() {
   return gulp.src('./dev/scss/main.scss')
-    .pipe( sourceMap.init() )
-    .pipe( sass( {outputStyle: 'compressed'} ).on( 'error', sass.logError ) )
-    .pipe( mmq( {log: true} ) )
-    .pipe( postcss( [autoprefixer, cssnano] ) )
+    .pipe(sourceMap.init())
+    .pipe(sass({
+      outputStyle: 'compressed'
+    }).on('error', sass.logError))
+    .pipe(mmq({
+      log: true
+    }))
+    .pipe(postcss([autoprefixer, cssnano]))
     // .pipe( gulp.dest( './public/' ) )
-    .pipe( browser.reload({stream: true}) )
-    .pipe( sourceMap.write() )
-    .pipe( gulp.dest( 'public/css/' ) );
+    .pipe(browser.reload({
+      stream: true
+    }))
+    .pipe(sourceMap.write())
+    .pipe(gulp.dest('public/css/'));
 }
 
 //CSS task without source maps
 function buildCSS() {
-  return gulp.src( './dev/scss/main.scss' )
-    .pipe( sass( {outputStyle: 'compressed'} ).on( 'error', sass.logError ) )
-    .pipe( mmq( {log: true} ) )
-    .pipe( postcss( [autoprefixer, cssnano] ) )
-    .pipe( gulp.dest( './build/css/' ) );
+  return gulp.src('./dev/scss/main.scss')
+    .pipe(sass({
+      outputStyle: 'compressed'
+    }).on('error', sass.logError))
+    .pipe(mmq({
+      log: true
+    }))
+    .pipe(postcss([autoprefixer, cssnano]))
+    .pipe(gulp.dest('./build/css/'));
 }
 
 //JS task with source maps
+// function devJS() {
+//   console.log('devJS');
+//   return gulp.src('dev/js/index.js')
+//   .pipe(webpack(require('./webpack.config.js'), compiler, function(err, stats) {
+//     /* Use stats to do more things if needed */
+//   }))
+//   .pipe(gulpif(process.env.NODE_ENV === "production", gulp.dest('./build/js/')))
+//   .pipe(gulpif(process.env.NODE_ENV === "development", gulp.dest('./public/js/')))
+// }
 function devJS() {
-  console.log('devJS');
-  return gulp.src('dev/js/index.js')
-  .pipe(webpack(require('./webpack.config.js'), compiler, function(err, stats) {
-    /* Use stats to do more things if needed */
-  }))
-  .pipe(gulpif(process.env.NODE_ENV === "production", gulp.dest('./build/js/')))
-  .pipe(gulpif(process.env.NODE_ENV === "development", gulp.dest('./public/js/')))
+  return gulp.src(['node_modules/babel-polyfill/dist/polyfill.js', './dev/js/*.js'])
+    .pipe(babel({
+      presets: ['@babel/preset-env'],
+      plugins: [
+        "@babel/plugin-proposal-class-properties",
+      ]
+    }))
+    .pipe(gulp.dest('./public/js/'))
 }
-
 
 // function publicJS() {
 //   return gulp.src( jsFiles, {base: 'dev/scripts'} )
@@ -114,45 +134,46 @@ function devJS() {
 
 //Optimize images
 function optimizeImages() {
-  return gulp.src( path.images.dev )
-    .pipe( image() )
-    .pipe( gulp.dest( path.images.public ) );
+  return gulp.src(path.images.dev)
+    .pipe(image())
+    .pipe(gulp.dest(path.images.public));
 }
 
 //Optimize svg
 function optimizeSVG() {
-  return gulp.src( path.svg.dev )
-    .pipe( svgmin() )
-    .pipe( gulp.dest( path.svg.public ) );
+  return gulp.src(path.svg.dev)
+    .pipe(svgmin())
+    .pipe(gulp.dest(path.svg.public));
 }
 
 //Copy fonts in public folder
 function font() {
-  return gulp.src( './' )
-    .pipe( gulp.dest( path.fonts.public ) );
+  return gulp.src('./')
+    .pipe(gulp.dest(path.fonts.public));
 }
 
 //HTML minification for public folder
 function htmlMin() {
-  return gulp.src( './dev/index.html' )
-    .pipe( html() )
-    .pipe( gulp.dest('./build/' ) );
+  return gulp.src('./dev/index.html')
+    .pipe(html())
+    .pipe(gulp.dest('./build/'));
 }
 
 //Before new build clean public folder
 function clean() {
-  return del( ['./build/'] );
+  return del(['./build/']);
 }
+
 function copyHTML() {
- return gulp.src('./dev/index.html').pipe(gulp.dest('./public/'));
+  return gulp.src('./dev/index.html').pipe(gulp.dest('./public/'));
 }
 
 function watch() {
-  gulp.watch( './dev/scss/*.scss', devCSS );
-  gulp.watch( './dev/js/*.js', gulp.parallel(devJS, browser.reload));
+  gulp.watch('./dev/scss/*.scss', devCSS);
 }
+
 function watchHTML() {
-  gulp.watch( './public/', browser.reload );
+  gulp.watch('./public/', browser.reload);
 }
 
 function browserSync() {
@@ -160,13 +181,14 @@ function browserSync() {
     server: "./public/"
   });
 
-  gulp.watch( './dev/index.html' ).on("change", gulp.parallel(copyHTML, watchHTML));
+  gulp.watch('./dev/index.html').on("change", gulp.parallel(copyHTML, watchHTML));
 
+  gulp.watch('./dev/js/*.js').on("change", gulp.parallel(devJS, browser.reload));
 }
 
-let build = gulp.series( clean, gulp.parallel( buildCSS, devJS, htmlMin ) );
+let build = gulp.series(clean, gulp.parallel(buildCSS, devJS, htmlMin));
 let beforeServer = gulp.parallel(copyHTML, devCSS, devJS);
-let dev = gulp.series( beforeServer, gulp.parallel( browserSync, watch ));
+let dev = gulp.series(beforeServer, gulp.parallel(browserSync, watch));
 
 //exports['name task for call in cli'] = nameFunctionTask
 exports.styles = devCSS;
