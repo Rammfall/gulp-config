080a8ae311b373e1e0b21803c733d6bb6d687874
AndreyBarchuk <andreyb@wizardsdev.com>
2020-04-21 13:17:59 +0300

    useless commit

--- app/src/js/main.js
+++ app/src/js/main.js
@@ -3,5 +3,5 @@ import 'webp-in-css/polyfill';
 export const reversArr = arr => arr.reverse();
 
 document.addEventListener('DOMContentLoaded', () => {
-  const reversedArr = reversArr([1,2,3]); 
+  reversArr([1,2,3]); 
 });
